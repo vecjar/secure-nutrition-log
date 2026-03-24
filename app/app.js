ce0ee1abc2ffd5dab882e8e7431a535a2a994244
Jarrod <jvecchiet@cybercx.com>
2026-03-24 20:08:31 +1100

    Modfied API Base URL

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://snlog-dev-func-01-bebdyfahfvd3efdh.australiaeast-01.azurewebsites.net/api';
+const API_BASE_URL = 'https://salmon-ground-0674e7100.1.azurestaticapps.net';
 
 const entryForm = document.getElementById('entryForm');
 const formMessage = document.getElementById('formMessage');
