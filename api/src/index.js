8363eef024489dc21826fa3230d13d55c2d0b645
Jarrod <jvecchiet@cybercx.com>
2026-03-25 14:49:04 +1100

    Changes to package.json and index.js for hardening backend

---
+++ api/src/index.js
@@ -0,0 +1,8 @@
+require('./functions/getCustomFoods');
+require('./functions/getTodayEntries');
+require('./functions/createCustomFood');
+require('./functions/createEntry');
+require('./functions/deleteCustomFood');
+require('./functions/deleteEntry');
+require('./functions/getGoals');
+require('./functions/saveGoals');
